f50c899f7f6a859bcdcf9cbfea4196ff5945d267
Esteban710U <2008esteban12@gmail.com>
2026-09-07 14:18:28 -0500

    implementacion de endpoint getEquipos

--- src/routes/equiposRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { TraerEquipos } from "../controllers/equiposController.js";
-
-const EquiposRouter = Router();
-
-EquiposRouter.get("/equipos", TraerEquipos)
-
-export default EquiposRouter
